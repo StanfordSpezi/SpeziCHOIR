6e7e664a9f61ac063b3ee3e78c19fd49ce9d9795
NikolaiMadlener <nikolai@madlener.io>
2025-02-18 15:29:50 +0100

    remove unused code in account keys

--- Sources/SpeziCHOIR/Account/AccountKeys.swift
+++ Sources/SpeziCHOIR/Account/AccountKeys.swift
@@ -14,129 +14,18 @@ import SpeziAccount
 import SwiftUI
 
 
-public enum PreferredCommunication: String, CaseIterable, Codable, Hashable, Sendable {
-    case email
-    case sms
-    
-    internal var internalType: Components.Schemas.Participant.contactPreferencePayload {
-        switch self {
-        case .email: .Email
-        case .sms: .Text
-        }
-    }
-    
-    internal init(internalType: Components.Schemas.Participant.contactPreferencePayload) {
-        self = switch internalType {
-        case .Email: .email
-        case .Text: .sms
-        }
-    }
-}
-
-private struct PreferredCommunicationDisplayView: DataDisplayView {
-    private let value: PreferredCommunication
-    
-    var body: some View {
-        LabeledContent("Preferred Communication") {
-            Text(value.rawValue)
-        }
-    }
-    
-    init(_ value: PreferredCommunication) {
-        self.value = value
-    }
-}
-
-private struct PreferredCommunicationEntryView: DataEntryView {
-    @Binding private var value: PreferredCommunication
-    
-    var body: some View {
-        Picker("Preferred Communication", selection: $value) {
-            ForEach(PreferredCommunication.allCases, id: \.self) { value in
-                Text(value.rawValue)
-            }
-        }
-    }
-    
-    init(_ value: Binding<PreferredCommunication>) {
-        self._value = value
-    }
-}
-
 extension AccountDetails {
-    @AccountKey(
-        name: "Organization",
-        category: .name,
-        as: String.self,
-        initial: .empty("")
-    )
-    public var organization: String?
-    
-    @AccountKey(
-        name: "Street",
-        category: .contactDetails,
-        as: String.self,
-        initial: .empty("")
-    )
-    public var street: String?
-    
-    @AccountKey(
-        name: "City",
-        category: .contactDetails,
-        as: String.self,
-        initial: .empty("")
-    )
-    public var city: String?
-    
-    @AccountKey(
-        name: "State",
-        category: .contactDetails,
-        as: String.self,
-        initial: .empty("")
-    )
-    public var state: String?
-    
-    @AccountKey(
-        name: "Postal Code",
-        category: .contactDetails,
-        as: String.self,
-        initial: .empty("")
-    )
-    public var postalCode: String?
-    
-    @AccountKey(
-        name: "Country",
-        category: .contactDetails,
-        as: String.self,
-        initial: .empty("")
-    )
-    public var country: String?
-    
+
     @AccountKey(
         name: "Phone",
         category: .contactDetails,
         as: String.self,
         initial: .empty("")
     )
-    public var phoneNumber: String?
     
-    @AccountKey(
-        name: "Preferred Communication",
-        category: .other,
-        as: PreferredCommunication.self,
-        initial: .default(.email),
-        displayView: PreferredCommunicationDisplayView.self,
-        entryView: PreferredCommunicationEntryView.self
-    )
-    public var preferredCommunication: PreferredCommunication?
+    public var phoneNumber: String?
 }
 
-@KeyEntry(\.organization)
-@KeyEntry(\.street)
-@KeyEntry(\.city)
-@KeyEntry(\.state)
-@KeyEntry(\.country)
-@KeyEntry(\.postalCode)
 @KeyEntry(\.phoneNumber)
-@KeyEntry(\.preferredCommunication)
+
 public extension AccountKeys {}
